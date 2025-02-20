6d4a6ce1886d94e0fb7aa7c8d4fba7c01bc38eae
Tapoban Ray <raytapoban@gmail.com>
2025-02-20 23:07:04 +0530

    Added Lottie and Rive animations

--- animations_1/lib/lottie_animations/lottie_animation_home.dart
+++ animations_1/lib/lottie_animations/lottie_animation_home.dart
@@ -1,5 +1,7 @@
 import 'package:animations_1/widgets/lesson_navigation_button.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class LottieAnimationHome extends StatelessWidget {
   const LottieAnimationHome({super.key});
@@ -16,11 +18,94 @@ class LottieAnimationHome extends StatelessWidget {
           children: [
             LessonNavigationButton(
               buttonText: "Lottie Animation",
-              navigateToPage: SizedBox(),
+              navigateToPage: LottieAnimationDemo(),
             ),
           ],
         ),
       ),
     );
+  }
+}
+
+enum LottieAnimationPaths {
+  bird(path: "assets/lottie_animations/bird.json"),
+  car(path: "assets/lottie_animations/car.json"),
+  tiger(path: "assets/lottie_animations/tiger.json");
+
+  final String path;
+
+  const LottieAnimationPaths({required this.path});
+}
+
+class LottieAnimationDemo extends StatefulWidget {
+  const LottieAnimationDemo({super.key});
+
+  @override
+  State<LottieAnimationDemo> createState() => _LottieAnimationDemoState();
+}
+
+class _LottieAnimationDemoState extends State<LottieAnimationDemo> {
+  late PageController _pageController;
+  int _currentPage = 0;
+
+  @override
+  void initState() {
+    _pageController = PageController(initialPage: 0);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Lottie Animation Demo"),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentPage,
+          onTap: (newIndex) {
+            _pageController.animateToPage(
+              newIndex,
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeIn,
+            );
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.animation_outlined),
+              label: "Animation 1",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.brightness_medium),
+              label: "Animation 2",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.car_rental),
+              label: "Animation 3",
+            ),
+          ],
+        ),
+        body: PageView.builder(
+          itemCount: LottieAnimationPaths.values.length,
+          controller: _pageController,
+          onPageChanged: (newPage) {
+            setState(() {
+              _currentPage = newPage;
+            });
+          },
+          itemBuilder: (context, index) => Center(
+            child: Lottie.asset(
+              LottieAnimationPaths.values[index].path,
+              repeat: true,
+              reverse: true,
+            ),
+          ),
+        ));
   }
 }
--- animations_1/lib/rive_animations/rive_animations_home.dart
+++ animations_1/lib/rive_animations/rive_animations_home.dart
@@ -1,5 +1,6 @@
 import 'package:animations_1/widgets/lesson_navigation_button.dart';
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
 
 class RiveAnimationsHome extends StatelessWidget {
   const RiveAnimationsHome({super.key});
@@ -16,9 +17,92 @@ class RiveAnimationsHome extends StatelessWidget {
           children: [
             LessonNavigationButton(
               buttonText: "Rive Animations",
-              navigateToPage: SizedBox(),
+              navigateToPage: RiveAnimationsDemo(),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+enum RiveAnimationPaths {
+  bear(path: "assets/rive_animations/bear.riv"),
+  girl(path: "assets/rive_animations/girl.riv"),
+  rating(path: "assets/rive_animations/rating.riv");
+
+  final String path;
+
+  const RiveAnimationPaths({
+    required this.path,
+  });
+}
+
+class RiveAnimationsDemo extends StatefulWidget {
+  const RiveAnimationsDemo({super.key});
+
+  @override
+  State<RiveAnimationsDemo> createState() => _RiveAnimationsDemoState();
+}
+
+class _RiveAnimationsDemoState extends State<RiveAnimationsDemo> {
+  late PageController _pageController;
+  int _currentPage = 0;
+
+  @override
+  void initState() {
+    _pageController = PageController(initialPage: 0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Rive Animations Demo"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentPage,
+        onTap: (newIndex) {
+          _pageController.animateToPage(
+            newIndex,
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeIn,
+          );
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.animation_outlined),
+            label: "Animation 1",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.brightness_medium),
+            label: "Animation 2",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.car_rental),
+            label: "Animation 3",
+          ),
+        ],
+      ),
+      body: PageView.builder(
+        controller: _pageController,
+        itemCount: RiveAnimationPaths.values.length,
+        onPageChanged: (newPage) {
+          setState(() {
+            _currentPage = newPage;
+          });
+        },
+        itemBuilder: (context, index) => Center(
+          child: RiveAnimation.asset(
+            RiveAnimationPaths.values[index].path,
+          ),
         ),
       ),
     );
